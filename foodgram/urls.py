2f9ab004559dd563019fee53e4902f065419239a
iapro <prokh68@yandex.ru>
2021-04-12 21:14:53 +1000

    refactor code

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
+
 from . import views
 
 handler404 = 'foodgram.views.page_not_found'  # noqa
