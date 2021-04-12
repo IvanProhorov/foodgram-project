034c709da553a0325672e686a2b06c0a7dc42851
iapro <prokh68@yandex.ru>
2021-04-12 20:31:56 +1000

    refactor code

--- foodgram/views.py
+++ foodgram/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic.base import TemplateView, View
+from django.views.generic.base import TemplateView
 
 
 class FoodgramAuthorView(TemplateView):
